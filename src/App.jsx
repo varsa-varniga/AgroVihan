09216febe7057980872b8cc26144dc059f63624a
SUDHARSANS18 <sudharsansankar1810@gmail.com>
2025-04-08 12:01:22 +0530

    Heropage,About,Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GoogleLogin from "./Authentication/GoogleLogin.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Login from './Authentication/Login.jsx';
+import AboutUs from './pages/AboutUs.jsx'
+import HeroPage from './pages/Heropage.jsx';
+import './index.css'; // ✅ THIS is correct
+import Footer from './Components/Footer.jsx';
+import Welcome from './pages/Welcome.jsx'
+
 
 
 function App() {
   return (
+
     <BrowserRouter>
       <Navbar />
       <Routes>
@@ -14,6 +21,10 @@ function App() {
         <Route path="/google-login" element={<GoogleLogin />} />
         {/* Add more routes if needed */}
       </Routes>
+      <HeroPage/>
+      <Welcome/>
+      <AboutUs/>
+      <Footer/>
 
       
     </BrowserRouter>
