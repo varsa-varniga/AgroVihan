5356ede32ccd9f3b098e180f4dc4cb3f65ef6676
varsa-varniga <varsavarniga05@gmail.com>
2025-04-08 20:16:47 +0530

    set up route

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GoogleLogin from "./Authentication/GoogleLogin.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Login from './Authentication/Login.jsx';
-import AboutUs from './pages/AboutUs.jsx'
+import AboutUs from './pages/AboutUs.jsx';
 import HeroPage from './pages/Heropage.jsx';
-import './index.css'; // ✅ THIS is correct
+import './index.css';
 import Footer from './Components/Footer.jsx';
-import Welcome from './pages/Welcome.jsx'
-
-
+import Welcome from './pages/Welcome.jsx';
+import WeatherDashboard from './Weatherpredict/WeatherDashboard.jsx';
 
 function App() {
   return (
-
     <BrowserRouter>
       <Navbar />
       <Routes>
-        
+        <Route path="/" element={<HeroPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/google-login" element={<GoogleLogin />} />
-        {/* Add more routes if needed */}
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/feature" element={<Welcome />} />
+        <WeatherDashboard/>
       </Routes>
-      <HeroPage/>
-      <Welcome/>
-      <AboutUs/>
-      <Footer/>
-
-      
+      <Footer />
     </BrowserRouter>
   );
 }
